6ed82dda362cec0be251f181c80bb63de25018e7
sir Random <random@amberskynet.org>
2025-11-13 20:27:41 +0300

    fix

--- src/modules/components/reputation.rs
+++ src/modules/components/reputation.rs
@@ -1,12 +1,6 @@
 use serde::Serialize;
 
-#[derive(Serialize, Clone, Copy)]
+#[derive(Serialize, Clone, Copy, Default)]
 pub struct Reputation {
     pub value: i32,
-}
-
-impl Default for Reputation {
-    fn default() -> Self {
-        Self { value: 0 }
-    }
 }
