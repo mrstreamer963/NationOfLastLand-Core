7870c9065602ebdcccc2d5aa07e034bd24f5ca60
mrstreamer963 <698lo7epy@mozmail.com>
2025-11-23 22:00:56 +0300

    update

--- core/src/modules/components/mod.rs
+++ core/src/modules/components/mod.rs
@@ -4,7 +4,7 @@ mod reputation;
 mod states;
 mod health;
 mod target_id;
-mod alert_type;
+mod unit_type;
 mod damage_type;
 
 pub use spatial::{MaxSpeed, Pos, Rot, Velocity, TargetPos};
@@ -12,5 +12,5 @@ pub use reputation::Reputation;
 pub use states::{IsMoving, IsStopped, IsWaitingTarget};
 pub use health::Health;
 pub use target_id::TargetId;
-pub use alert_type::UnitType;
+pub use unit_type::UnitType;
 pub use damage_type::DamageType;
