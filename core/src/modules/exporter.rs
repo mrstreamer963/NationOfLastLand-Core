d18ff4bdda75c2805ad0a6bef3e65991317fa7a8
mrstreamer963 <698lo7epy@mozmail.com>
2025-11-25 20:26:03 +0300

    temp fixes

--- core/src/modules/exporter.rs
+++ core/src/modules/exporter.rs
@@ -2,52 +2,81 @@ use crate::modules::components::{DamageType, EntityType, Health, MaxSpeed, Pos, 
 use crate::modules::markers::{Alert, Vehicle};
 
 use crate::modules::state::State;
-use hecs::World;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use hecs::{serialize::row::*, World, EntityRef};
+use serde::{Serialize, ser::SerializeMap};
 
-type UnitsType = Vec<Vec<Value>>;
-
-#[derive(Serialize, Deserialize)]
 struct ExportData {
-    units: UnitsType,
+    units: Vec<serde_json::Value>,
     state: State,
 }
 
+impl Serialize for ExportData {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut map = serializer.serialize_map(None)?;
+        map.serialize_entry("units", &self.units)?;
+        map.serialize_entry("state", &self.state)?;
+        map.end()
+    }
+}
+
+#[derive(Serialize)]
+enum ComponentId {
+    Pos,
+    EntityType,
+    Health,
+    Velocity,
+    Rot,
+    MaxSpeed,
+    Target,
+    Reputation,
+    UnitState,
+    TargetId,
+    DamageType,
+    Alert,
+    Vehicle,
+}
+
+struct Context;
+
+impl SerializeContext for Context {
+    fn serialize_entity<S>(
+        &mut self,
+        entity: EntityRef<'_>,
+        mut map: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: SerializeMap,
+    {
+        try_serialize::<Pos, _, _>(&entity, &ComponentId::Pos, &mut map)?;
+        try_serialize::<EntityType, _, _>(&entity, &ComponentId::EntityType, &mut map)?;
+        try_serialize::<Health, _, _>(&entity, &ComponentId::Health, &mut map)?;
+        try_serialize::<Velocity, _, _>(&entity, &ComponentId::Velocity, &mut map)?;
+        try_serialize::<Rot, _, _>(&entity, &ComponentId::Rot, &mut map)?;
+        try_serialize::<MaxSpeed, _, _>(&entity, &ComponentId::MaxSpeed, &mut map)?;
+        try_serialize::<TargetPos, _, _>(&entity, &ComponentId::Target, &mut map)?;
+        try_serialize::<Reputation, _, _>(&entity, &ComponentId::Reputation, &mut map)?;
+        try_serialize::<UnitState, _, _>(&entity, &ComponentId::UnitState, &mut map)?;
+        try_serialize::<TargetId, _, _>(&entity, &ComponentId::TargetId, &mut map)?;
+        try_serialize::<DamageType, _, _>(&entity, &ComponentId::DamageType, &mut map)?;
+        map.serialize_entry(&ComponentId::Alert, &entity.has::<Alert>())?;
+        map.serialize_entry(&ComponentId::Vehicle, &entity.has::<Vehicle>())?;
+        map.end()
+    }
+}
+
 pub fn export_to_json(world: &World, state: &State) -> String {
-    let mut units: UnitsType = Vec::new();
+    let mut units = Vec::new();
 
-    let component_descriptions: Vec<(&str, Box<dyn Fn(&World, hecs::Entity) -> Option<Value>>)> = vec![
-        ("health", Box::new(|world, id| world.get::<&Health>(id).ok().map(|c| serde_json::to_value(*c).unwrap()))),
-        ("velocity", Box::new(|world, id| world.get::<&Velocity>(id).ok().map(|c| serde_json::to_value(*c).unwrap()))),
-        ("rot", Box::new(|world, id| world.get::<&Rot>(id).ok().map(|c| serde_json::to_value(*c).unwrap()))),
-        ("max_speed", Box::new(|world, id| world.get::<&MaxSpeed>(id).ok().map(|c| serde_json::to_value(*c).unwrap()))),
-        ("target", Box::new(|world, id| world.get::<&TargetPos>(id).ok().map(|c| serde_json::to_value(*c).unwrap()))),
-        ("reputation", Box::new(|world, id| world.get::<&Reputation>(id).ok().map(|c| serde_json::to_value(*c).unwrap()))),
-        ("unit_state", Box::new(|world, id| world.get::<&UnitState>(id).ok().map(|c| serde_json::to_value(*c).unwrap()))),
-        ("target_id", Box::new(|world, id| world.get::<&TargetId>(id).ok().map(|c| serde_json::to_value(*c).unwrap()))),
-        ("damage_type", Box::new(|world, id| world.get::<&DamageType>(id).ok().map(|c| serde_json::to_value(*c).unwrap()))),
-        ("alert", Box::new(|world, id| world.get::<&Alert>(id).ok().map(|_| Value::Bool(true)))),
-        ("vehicle", Box::new(|world, id| world.get::<&Vehicle>(id).ok().map(|_| Value::Bool(true)))),
-    ];
-
-    // Выборка всех units с Pos и EntityType
-    for (_id, (pos, entity_type)) in world.query::<(&Pos, &EntityType)>().iter() {
-        let id_val = Value::Number(_id.id().into());
-        let pos_val = serde_json::to_value(*pos).unwrap();
-        let unit_type_val = serde_json::to_value(*entity_type).unwrap();
-
-        let mut row = vec![id_val, pos_val, unit_type_val];
-        for (name, getter) in &component_descriptions {
-            if let Some(val) = getter(world, _id) {
-                row.push(val);
-            } else if *name == "alert" || *name == "vehicle" {
-                row.push(Value::Bool(false));
-            } else {
-                row.push(Value::Null);
-            }
-        }
-        units.push(row);
+    for (id, (_pos, _entity_type)) in world.query::<(&Pos, &EntityType)>().iter() {
+        let entity = world.entity(id).unwrap();
+        let unit_val = serde_json::to_value(UnitExport {
+            id: id.id(),
+            entity,
+        }).unwrap();
+        units.push(unit_val);
     }
 
     let data = ExportData {
@@ -55,4 +84,21 @@ pub fn export_to_json(world: &World, state: &State) -> String {
         state: state.clone(),
     };
     serde_json::to_string(&data).unwrap()
+}
+
+struct UnitExport<'a> {
+    id: u32,
+    entity: EntityRef<'a>,
+}
+
+impl Serialize for UnitExport<'_> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut map = serializer.serialize_map(None)?;
+        map.serialize_entry("id", &(self.id as u64))?;
+        let mut context = Context;
+        context.serialize_entity(self.entity.clone(), map)
+    }
 }
