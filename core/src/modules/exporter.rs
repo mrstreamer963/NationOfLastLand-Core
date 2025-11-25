1db9f4915161d033e37d2af8c5df71813dc50874
mrstreamer963 <698lo7epy@mozmail.com>
2025-11-25 10:31:18 +0300

    temporary fix

--- core/src/modules/exporter.rs
+++ core/src/modules/exporter.rs
@@ -1,10 +1,27 @@
-use crate::modules::components::{EntityType, Health, Pos, TargetPos, Velocity};
+use crate::modules::components::{DamageType, EntityType, Health, MaxSpeed, Pos, Reputation, Rot, TargetId, TargetPos, UnitState, Velocity};
+use crate::modules::markers::{Alert, Vehicle};
 
 use crate::modules::state::State;
 use hecs::World;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
+
+struct ComponentValue {
+    name: String,
+    value: Value,
+}
+
+impl ComponentValue {
+    fn new<T: Serialize>(name: &str, component: &T) -> Self {
+        Self {
+            name: name.to_string(),
+            value: serde_json::to_value(component).unwrap(),
+        }
+    }
+}
+
+type ComponentGetter = Box<dyn Fn(&World, hecs::Entity) -> Option<ComponentValue>>;
 
 #[derive(Serialize, Deserialize)]
 struct ExportData {
@@ -15,6 +32,30 @@ struct ExportData {
 pub fn export_to_json(world: &World, state: &State) -> String {
     let mut units = Vec::new();
 
+    let component_getters: Vec<ComponentGetter> = vec![
+        Box::new(|world, id| world.get::<&Health>(id).ok().map(|c| ComponentValue::new("health", &*c))),
+        Box::new(|world, id| world.get::<&Velocity>(id).ok().map(|c| ComponentValue::new("velocity", &*c))),
+        Box::new(|world, id| world.get::<&Rot>(id).ok().map(|c| ComponentValue::new("rot", &*c))),
+        Box::new(|world, id| world.get::<&MaxSpeed>(id).ok().map(|c| ComponentValue::new("max_speed", &*c))),
+        Box::new(|world, id| world.get::<&TargetPos>(id).ok().map(|c| ComponentValue::new("target", &*c))),
+        Box::new(|world, id| world.get::<&Reputation>(id).ok().map(|c| ComponentValue::new("reputation", &*c))),
+        Box::new(|world, id| world.get::<&UnitState>(id).ok().map(|c| ComponentValue::new("unit_state", &*c))),
+        Box::new(|world, id| world.get::<&TargetId>(id).ok().map(|c| ComponentValue::new("target_id", &*c))),
+        Box::new(|world, id| world.get::<&DamageType>(id).ok().map(|c| ComponentValue::new("damage_type", &*c))),
+        Box::new(|world, id| {
+            world.get::<&Alert>(id).ok().map(|_| ComponentValue {
+                name: "alert".to_string(),
+                value: Value::Bool(true),
+            })
+        }),
+        Box::new(|world, id| {
+            world.get::<&Vehicle>(id).ok().map(|_| ComponentValue {
+                name: "vehicle".to_string(),
+                value: Value::Bool(true),
+            })
+        }),
+    ];
+
     // Выборка всех alerts
     for (_id, (pos, alert_type )) in world.query::<(&Pos, &EntityType)>().iter() {
         let mut alert_data = HashMap::from([
@@ -23,26 +64,11 @@ pub fn export_to_json(world: &World, state: &State) -> String {
             ("unit_type".to_string(), serde_json::to_value(*alert_type).unwrap()),
         ]);
 
-        if let Ok(health) = world.get::<&Health>(_id) {
-            alert_data.insert("health".to_string(), serde_json::to_value(*health).unwrap());
+        for getter in &component_getters {
+            if let Some(cv) = getter(world, _id) {
+                alert_data.insert(cv.name, cv.value);
+            }
         }
-
-        // if let Ok(velocity) = world.get::<&Velocity>(_id) {
-        //     alert_data.insert("velocity".to_string(), serde_json::to_value(*velocity).unwrap());
-        // }
-
-        // if let Ok(rot) = world.get::<&Velocity>(_id) {
-        //     alert_data.insert("rot".to_string(), serde_json::to_value(*rot).unwrap());
-        // }
-
-        // if let Ok(max_speed) = world.get::<&Velocity>(_id) {
-        //     alert_data.insert("max_speed".to_string(), serde_json::to_value(*max_speed).unwrap());
-        // }
-        
-        // // Add optional components
-        // if let Ok(target) = world.get::<&TargetPos>(_id) {
-        //     alert_data.insert("target".to_string(), serde_json::to_value(*target).unwrap());
-        // }
 
         units.push(alert_data);
     }
