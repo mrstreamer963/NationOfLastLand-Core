9958e4e036c23d5c5eac580f3126322bb6e27a56
mrstreamer963 <698lo7epy@mozmail.com>
2025-12-07 09:33:23 +0300

    temp fixes

---
+++ core/src/modules/systems/attack_processor.rs
@@ -0,0 +1,7 @@
+use hecs::World;
+
+use crate::descriptions::Descriptions;
+
+pub fn attack_process(world: &mut World, descriptions: &Descriptions) {
+
+}
--- core/src/modules/systems/mod.rs
+++ core/src/modules/systems/mod.rs
@@ -1,1 +1,2 @@
 pub mod ai_vehicle;
+pub mod attack_processor;
