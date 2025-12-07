ba059db8828f01f1c2b59c674523a56acf3d918c
mrstreamer963 <698lo7epy@mozmail.com>
2025-12-07 09:31:00 +0300

    updates

--- core/examples/ex_attack_vehicles.rs
+++ core/examples/ex_attack_vehicles.rs
@@ -6,16 +6,6 @@ use nation_of_last_land_core::modules::markers::{Alert, AttackEvent, IsTargetNea
 fn main() {
     let mut core = Core::new();
 
-    // Create a vehicle
-    // let vehicle = core.create_vehicle_from_yaml("VEHICLE_CAR", Pos { x: 0.0, y: 0.0 }).unwrap();
-
-    // Create an item with interactions
-    // let item = core.create_item_from_yaml("ITEM_CLEANER", Pos { x: 0.0, y: 0.0 }).unwrap();
-
-    // Attach item to vehicle
-    // core.attach(vehicle, item, "front_left").unwrap();
-
-
     // Update the world until the vehicle attacks
     println!("Updating world to simulate vehicle movement and attacks:");
     for i in 1..=100 {
